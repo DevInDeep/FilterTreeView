7c29462da4aa1ff59cc6a5ea2cc539c0f7151eb2
Vancho Pavlevski <pavlevski.vanco@outlook.com>
2025-08-05 19:04:40 +0200

    Add project files.

---
+++ FilterTreeView/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace FilterTreeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e) => Debug.WriteLine(txtSearch.Text);
+    }
+}
