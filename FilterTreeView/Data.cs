511789793620b3bfcdeef1c462cae8078974dc6e
Vancho Pavlevski <pavlevski.vanco@outlook.com>
2025-08-05 19:12:12 +0200

    Add Test Data

---
+++ FilterTreeView/Data.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Numerics;
+
+namespace FilterTreeView
+{
+    internal static class Data
+    {
+        internal static IEnumerable<string> FetchTestData()
+        {
+            return new string[]
+            {"Albania", "Andorra", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Macedonia", "Malta", "Moldova", "Monaco", "Montenegro", "Netherlands", "Norway", "Poland", "Portugal", "Romania", "Russia", "San Marino", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Ukraine", "United Kingdom" };
+        }
+    }
+}
--- FilterTreeView/MainWindow.xaml.cs
+++ FilterTreeView/MainWindow.xaml.cs
@@ -10,6 +10,9 @@ namespace FilterTreeView
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e) => treeView.ItemsSource = Data.FetchTestData();
         private void txtSearch_TextChanged(object sender, TextChangedEventArgs e) => Debug.WriteLine(txtSearch.Text);
+
+        
     }
 }
